340fe3fb986366ccbb955c65295ff1ecd0542e7e
Coryb <y-crbnz25@outlook.com>
2026-07-14 21:09:31 -0600

    Se agrega funcionalidad del metodo "comprarSnack"

--- src/maquina_snacks/MaquinaSnacks.java
+++ src/maquina_snacks/MaquinaSnacks.java
@@ -11,12 +11,62 @@ public class MaquinaSnacks {
 
     public static void maquinaSnacks(){
         var salir = false;
-        var console = new Scanner(System.in);
+        var consola = new Scanner(System.in);
         //Creamos la lista de productos de tipo Snack
         List<Snack> productos = new ArrayList<>();
 
         System.out.println("***Maquina de Snacks***");
         Snacks.mostrarSnack(); //Mostrar inventario de Snacks disponibles
 
+        while(!salir){
+            try{
+                var opcion = mostrarMenu(consola);
+                salir = ejecutarOpciones(opcion, consola, productos);
+            }catch(Exception e){
+                System.out.println("Ocurrio un error: " + e.getMessage());
+            }
+            finally {
+                System.out.println();
+            }
+        }
+
+    }
+
+    private static int mostrarMenu(Scanner consola){
+        System.out.println("""
+                Menu:
+                1. Comprar Snack
+                2. Mostrar ticket
+                3. Agregar nuevo Snack
+                4. Salir
+                Elige una opcion: \s""");
+        return consola.nextInt();
+    }
+
+    private static boolean ejecutarOpciones(int opcion, Scanner consola, List<Snack> productos){
+        var salir = false;
+        switch (opcion){
+            case 1 -> comprarSnack(consola, productos);
+        }
+        return salir;
+    }
+
+    private static void comprarSnack(Scanner consola, List<Snack> productos){
+        System.out.print("Que Snack quieres comprar (ID) : ");
+        int idSnack = consola.nextInt();
+        //Validar que el snack exista en la lista de Snacks
+        var snackEncontrado = false;
+        for(var snack : Snacks.getSnacks()){
+            if(idSnack == snack.getIdSnack()){
+                //Se agrega Snack a la lista de productos
+                productos.add(snack);
+                System.out.println("Ok, Snack agregado: " + snack);
+                snackEncontrado = true;
+                break;//Termina la iteracion del ciclo for cuando se encuentre el snack
+            }
+        }
+        if(!snackEncontrado){
+            System.out.println("Id de snack no encontrado: " + idSnack);
+        }
     }
 }
